c08f35fb83c5bb8fdc84e3444a5144b251da4511
AnuragJha1954 <anuragjha@jklu.edu.in>
2026-03-12 02:34:40 +0530

    modified the UI

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,16 +12,14 @@ class SignUpForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         for name, field in self.fields.items():
             field.widget.attrs.update({
-                'class': 'w-full h-14 px-5 rounded-2xl border border-slate-300 '
-                         'bg-white text-slate-800 placeholder-slate-400 '
-                         'focus:outline-none focus:ring-4 focus:ring-cyan-200 '
-                         'focus:border-cyan-500 transition',
-                'placeholder': field.label,
+                "class": "field-input",
+                "placeholder": field.label
             })
-            field.label = ""
 
+            field.label = ""
 
 class LoginForm(AuthenticationForm):
 
